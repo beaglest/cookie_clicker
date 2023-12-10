1d16329abfe9e160abf6d3f31dd7dbf7668b2eb3
beaglest <39458964+beaglest@users.noreply.github.com>
2023-12-10 20:04:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import selenium.common.exceptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+AWARDS = ["buyTime machine", "buyPortal", "buyAlchemy lab", "buyShipment", "buyFactory", "buyGrandma", "buyCursor"]
+
+
+def return_int(s):
+
+    z = s.split(",")
+    y = "".join(z)
+    m = int(y)
+    return m
+
+
+def get_reward(drv):
+
+    print("in get reward")
+    for award in AWARDS:
+        money1 = drv.find_element(By.ID, "money")
+        a = drv.find_element(By.ID, award)
+        c = a.get_attribute("class")
+        x = a.text.split("\n")
+        m1 = x[0].split("-")
+        # print("4")
+        cost = return_int(m1[1])
+        money = return_int(money1.text)
+
+        if not c:
+            if money > cost:
+                print(a.text)
+                # try:
+                #     a.click()
+                # except selenium.common.exceptions.StaleElementReferenceException:
+                #     print("pas geciyorum")
+
+                # print(f"money: {money} and cost is {m}")
+        del a, c, x
+        # del a, c, x, m1, cost, money
+
+
+driver = webdriver.Chrome()
+# driver.get("https://www.google.com/")
+#
+# chrome_options = webdriver.ChromeOptions()
+# chrome_driver_path = '/Users/metinkoc/Downloads/chromedriver-mac-arm64/chromedriver'
+# # chrome_options.add_experimental_option("detach", True)
+#
+# # driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(chrome_driver_path)
+
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+cookie = driver.find_element(By.ID, "cookie")
+
+
+start_time = time.time()
+
+while (time.time() - start_time) < 300:
+    cookie.click()
+    if (5.0 - (time.time() - start_time) % 5.0) < 0.2:
+        get_reward(driver)
